0033989e60784432d15c8a08db40143a76aee080
Ajay Deekonda <133995637+Ajaydeekonda@users.noreply.github.com>
2024-10-25 12:44:07 +0530

    fixed cors error

--- server.js
+++ server.js
@@ -8,9 +8,7 @@ const cors = require('cors');
 
 const app = express();
 
-app.use(cors({
-    origin:["http://localhost:3000/","https://localhost:3000/"]
-}));
+app.use(cors());
 // Connect to MongoDB
 connectDB();
 
